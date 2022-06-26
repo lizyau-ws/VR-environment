8817b8b8871d1ce90e4a666e2574467f4f17839c
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-27 00:09:47 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -33,9 +33,9 @@ function grow(){
 }
 
 function decrease(){
-	myOtherBox.object3D.scale.x *= 0.3;
-	myOtherBox.object3D.scale.y *= 0.3;
-	myOtherBox.object3D.scale.z *= 0.3;
+	myOtherBox.object3D.scale.x *= 0.6;
+	myOtherBox.object3D.scale.y *= 0.6;
+	myOtherBox.object3D.scale.z *= 0.6;
 }
 
 myOtherBox.addEventListener('mouseleave', function(){
@@ -49,13 +49,5 @@ myOtherBox.addEventListener('click', function(){ // uses a fuse
 	console.log('grew');
 });
 
-myBox.addEventListener('click', function(){ // uses a fuse
-	function spin2(){
-		myBox.object3D.rotation.x += rotationSpeed;
-		myBox.object3D.rotation.y += rotationSpeed;
-		myBox.object3D.rotation.z += rotationSpeed/2;
-		//console.log(myOtherBox.object3D.rotation);
-	}
-	setInterval(spin2, 10);
-});
+
 
