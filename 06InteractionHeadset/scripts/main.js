4e4cd38230b42a1dfacf06d0d099e8d183582f56
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-26 22:31:15 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -22,13 +22,37 @@ myOtherBox.addEventListener('mouseleave', function(){
 });
 
 
+myBox.addEventListener('mouseenter', function(){
+	rotationSpeed = 0.01;
+	console.log('entered');
+});
+
+myBox.addEventListener('mouseleave', function(){
+	rotationSpeed = 0.001;
+	console.log('left');
+});
+
+var ggspeed = 0.05;
+
+function ggspeed(){
+	myOtherBox.object3D.rotation.x *= ggspeed;
+	myOtherBox.object3D.rotation.y *= ggspeed;
+	myOtherBox.object3D.rotation.z *= ggspeed/2;
+	console.log(myOtherBox.object3D.scale);
+}
+
+myOtherBox.addEventListener('click', function(){ // uses a fuse
+	ggspeed();
+	console.log('gg');
+});
+
 
 
 /*
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time. 
  */
 
-var growspeed = 5;
+var growspeed = 1.2;
 
 function grow(){
 	myOtherBox.object3D.scale.x *= growspeed;
