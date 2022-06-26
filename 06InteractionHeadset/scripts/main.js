c4667952f3abfcb4aa2b27154ce8298641479f16
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-26 22:48:26 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -1,13 +1,11 @@
 
 var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
-var myOtherBox = document.getElementById('myBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.y += rotationSpeed;
 	myOtherBox.object3D.rotation.z += rotationSpeed/2;
-
 	//console.log(myOtherBox.object3D.rotation);
 }
 setInterval(spin, 10);
@@ -24,46 +22,22 @@ myOtherBox.addEventListener('mouseleave', function(){
 });
 
 
-myBox.addEventListener('mouseenter', function(){
-	rotationSpeed = 0.01;
-	console.log('entered');
-});
-
-myBox.addEventListener('mouseleave', function(){
-	rotationSpeed = 0.001;
-	console.log('left');
-});
-
-var ggspeed = 0.05;
-
-function ggspeed(){
-	myBox.object3D.rotation.x *= ggspeed;
-	myBox.object3D.rotation.y *= ggspeed;
-	myBox.object3D.rotation.z *= ggspeed/2;
-	console.log(myOtherBox.object3D.scale);
-}
-
-myBox.addEventListener('click', function(){ // uses a fuse
-	ggspeed();
-	console.log('gg');
-});
-
 
 
 /*
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time. 
  */
 
-var growspeed = 1.2;
+// var growspeed = 1.4;
 
-function grow(){
-	myOtherBox.object3D.scale.x *= growspeed;
-	myOtherBox.object3D.scale.y *= growspeed;
-	myOtherBox.object3D.scale.z *= growspeed;
-	console.log(myOtherBox.object3D.scale);
-}
+// function grow(){
+// 	myOtherBox.object3D.scale.x *= growspeed;
+// 	myOtherBox.object3D.scale.y *= growspeed;
+// 	myOtherBox.object3D.scale.z *= growspeed;
+// 	//console.log(myOtherBox.object3D.scale);
+// }
 
-myOtherBox.addEventListener('click', function(){ // uses a fuse
-	grow();
-	console.log('grew');
-});
+// myOtherBox.addEventListener('click', function(){ // uses a fuse
+// 	grow();
+// 	console.log('grew');
+// });
