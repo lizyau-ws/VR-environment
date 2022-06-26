f7e1e33197578ebd58f19189ccd68e859bc39ca6
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-26 21:50:23 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -31,13 +31,13 @@ myOtherBox.addEventListener('mouseleave', function(){
 // var growspeed = 1.2;
 
 // function grow(){
-// 	myOtherBox.object3D.scale.x *= growspeed;
-// 	myOtherBox.object3D.scale.y *= growspeed;
-// 	myOtherBox.object3D.scale.z *= growspeed;
-// 	//console.log(myOtherBox.object3D.scale);
-// }
+	myOtherBox.object3D.scale.x *= growspeed;
+	myOtherBox.object3D.scale.y *= growspeed;
+	myOtherBox.object3D.scale.z *= growspeed;
+	//console.log(myOtherBox.object3D.scale);
+}
 
-// myOtherBox.addEventListener('click', function(){ // uses a fuse
-// 	grow();
-// 	console.log('grew');
-// });
+myOtherBox.addEventListener('click', function(){ // uses a fuse
+	grow();
+	console.log('grew');
+});
