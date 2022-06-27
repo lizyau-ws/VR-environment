9f382611e4d483fc77b5fbbb87a95ff53c5e3a95
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-27 16:11:43 +0200

    Update script2.js

--- 06InteractionHeadset/scripts/script2.js
+++ 06InteractionHeadset/scripts/script2.js
@@ -1,6 +1,7 @@
 
 var rotationSpeed = 0.001;
 var myOtherBox = document.getElementById('myOtherBox');
+var myBox = document.getElementById('myBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
