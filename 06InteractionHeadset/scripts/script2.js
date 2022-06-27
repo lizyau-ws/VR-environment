1a5f25ed0f9288056ed7b3b8828a723d64465541
lizyau-ws <78273836+lizyau-ws@users.noreply.github.com>
2022-06-27 16:05:36 +0200

    Update script2.js

--- 06InteractionHeadset/scripts/script2.js
+++ 06InteractionHeadset/scripts/script2.js
@@ -41,3 +41,21 @@ myOtherBox.addEventListener('click', function(){ // uses a fuse
 	grow();
 	console.log('grew');
 });
+
+function grow2(){
+	myBox.object3D.scale.x *= growspeed;
+	myBox.object3D.scale.y *= growspeed;
+	myBox.object3D.scale.z *= growspeed;
+	
+	rotationSpeed = 0.001;
+	myOtherBox.object3D.scale.x *= 0.5;
+	myOtherBox.object3D.scale.y *= 0.5;
+	myOtherBox.object3D.scale.z *= 0.5;
+	
+	//console.log(myOtherBox.object3D.scale);
+}
+
+myBox.addEventListener('click', function(){ // uses a fuse
+	grow2();
+	console.log('grew2');
+});
